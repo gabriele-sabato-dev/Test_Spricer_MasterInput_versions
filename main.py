f6c38d2918da281de6d8bfba63fbb2482f9cee91
Gabriele Sabato <gabriele.sabato at home24.de>
2020-11-30 17:30:48 +0100

    New file name

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ def run_check(column_number, files,path='/tmp/'):
 if __name__ == '__main__':
     md5_val_dic = {}
 
-    files_names = ['itemMasterPart1_20201125.csv000','itemMasterPart1_v2_20201125.csv000']
+    files_names = ['itemMasterPart1.csv000','itemMasterPart1_v2.csv000']
     for i in range(1,48+1):
         print(i)
         md5_val_dic[i]  = run_check(i,files_names)
     #print(md5_val_dic)
     for key in md5_val_dic.keys():
         if md5_val_dic[key][0] != md5_val_dic[key][1]:
-            print(f"Column {key} differ! ")
+            print(f"Column {key} differ!")
